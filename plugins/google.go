0a4170f71d5d95766e74fbb24d816c7457417f4b
ibrokemypie <conor@codenix.org>
2018-03-18 19:28:39 +1100

    Use embed for search results

--- plugins/google.go
+++ plugins/google.go
@@ -15,6 +15,7 @@ import (
 const urlBase = "https://www.googleapis.com/customsearch/v1?q="
 
 var queries = make(map[string]Query)
+var colour = 0xe75c5c
 
 type result struct {
 	number       int
@@ -54,8 +55,18 @@ func buttons(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
 			}
 			queries[m.MessageID] = Query{author: queries[m.MessageID].author, messageid: queries[m.MessageID].messageid, number: i, results: queries[m.MessageID].results, returned: queries[m.MessageID].returned}
 
-			returnString := strconv.Itoa(queries[m.MessageID].results[queries[m.MessageID].number].number) + "/" + strconv.Itoa(queries[m.MessageID].returned) + "\n" + queries[m.MessageID].results[queries[m.MessageID].number].title + "\n" + queries[m.MessageID].results[queries[m.MessageID].number].snippet + "\n<" + queries[m.MessageID].results[queries[m.MessageID].number].formattedUrl + ">"
-			s.ChannelMessageEdit(m.ChannelID, m.MessageID, returnString)
+			embed := discordgo.MessageEmbed{
+				Title:       queries[m.MessageID].results[queries[m.MessageID].number].title,
+				Description: queries[m.MessageID].results[queries[m.MessageID].number].snippet,
+				Color:       colour,
+				Footer:      &discordgo.MessageEmbedFooter{Text: strconv.Itoa(queries[m.MessageID].results[queries[m.MessageID].number].number) + "/" + strconv.Itoa(queries[m.MessageID].returned)},
+				URL:         queries[m.MessageID].results[queries[m.MessageID].number].formattedUrl,
+			}
+			_, err := s.ChannelMessageEditEmbed(m.ChannelID, m.MessageID, &embed)
+			if err != nil {
+				s.ChannelMessageSend(m.ChannelID, err.Error())
+				return
+			}
 		}
 	}
 }
@@ -84,7 +95,7 @@ func Search(q string, n int, apiKey string, engineID string, d *discordgo.Sessio
 	_, err = jsonparser.ArrayEach(body, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 		title, err := jsonparser.GetString(value, "title")
 		snippet, err := jsonparser.GetString(value, "snippet")
-		formattedUrl, err := jsonparser.GetString(value, "formattedUrl")
+		url, err := jsonparser.GetString(value, "link")
 		if err != nil {
 			d.ChannelMessageSend(m.ChannelID, err.Error())
 			return
@@ -92,21 +103,29 @@ func Search(q string, n int, apiKey string, engineID string, d *discordgo.Sessio
 
 		returned++
 
-		result := result{number: returned, title: title, snippet: snippet, formattedUrl: formattedUrl}
+		result := result{number: returned, title: title, snippet: snippet, formattedUrl: url}
 		if err != nil {
 			panic(err.Error())
 		}
 		results = append(results, result)
 	}, "items")
 
-	var returnString string
-
 	if !(len(results) > 0) {
 		d.ChannelMessageSend(m.ChannelID, "No results.")
 		return
 	}
-	returnString = strconv.Itoa(results[0].number) + "/" + strconv.Itoa(returned) + "\n" + results[0].title + "\n" + results[0].snippet + "\n<" + results[0].formattedUrl + ">"
-	message, _ := d.ChannelMessageSend(m.ChannelID, returnString)
+	embed := discordgo.MessageEmbed{
+		Title:       results[0].title,
+		Description: results[0].snippet,
+		Color:       colour,
+		Footer:      &discordgo.MessageEmbedFooter{Text: strconv.Itoa(results[0].number) + "/" + strconv.Itoa(returned)},
+		URL:         results[0].formattedUrl,
+	}
+	message, err := d.ChannelMessageSendEmbed(m.ChannelID, &embed)
+	if err != nil {
+		d.ChannelMessageSend(m.ChannelID, err.Error())
+		return
+	}
 
 	d.MessageReactionAdd(message.ChannelID, message.ID, "⬅")
 	d.MessageReactionAdd(message.ChannelID, message.ID, "➡")
